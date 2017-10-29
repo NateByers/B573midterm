e77c173f50083462f2a832b7db368d29a4dd888c
Nate Byers <natebyers@gmail.com>
2017-10-29 00:20:36 +0000

    got headers

--- read_data.R
+++ read_data.R
@@ -1,1 +1,20 @@
-lines <- readLines("data/BIOGRID-ORGANISM-Homo_sapiens-3.1.91.tab.txt")
+library(dplyr)
+
+aliases <- read.table("data/BIOGRID-ORGANISM-Homo_sapiens-3.1.91.tab.txt",
+                 stringsAsFactors = FALSE, skip = 35, header = TRUE, sep = "\t",
+                 comment.char = "")
+
+
+interactions <- read.table("data/gene_association.goa_human.txt",
+                           stringsAsFactors = FALSE, skip = 23, header = FALSE, sep = "\t",
+                           comment.char = "")
+
+# README is here ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/HUMAN/README
+
+headers <- readLines("ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/HUMAN/README")
+headers <-headers[(grep("^GAF2.1", headers) + 3):(grep("^GPAD1.1", headers) - 2)]
+headers <- sub("\\t\\d{1,2}\\s{1,7}", "", headers)
+headers <- gsub("\\(|\\)|:", "_", headers)
+headers <- gsub(" ", "", headers)
+
+names(interactions) <- headers
