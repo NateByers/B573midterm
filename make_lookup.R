c295c2c56fbb12bdf1d74e1ce375763937430979
Nate Byers <natebyers@gmail.com>
2017-10-29 17:52:57 +0000

    make lookup

---
+++ make_lookup.R
@@ -0,0 +1,23 @@
+make_interaction_lookup <- function(interactions = get_interactions()) {
+  # the %>% function is from the magrittr package, which is imported with the dplyr package
+
+  lookup <- lapply(c("A", "B"), function(x, interactions) {
+    # x = "A"
+    
+    interactions %>%
+      dplyr::select(ends_with(x)) %>%
+      dplyr::rename_all(funs(sub(paste0("_", x), "", .))) %>%
+      dplyr::mutate(ALIASES = paste(OFFICIAL_SYMBOL, ALIASES_FOR, sep = "|")) %>%
+      dplyr::select(OFFICIAL_SYMBOL, ALIASES) %>%
+      tidyr::separate_rows(ALIASES, sep = "\\|") %>%
+      dplyr::distinct() 
+    
+  }, interactions = interactions)
+  
+  lookup <- Reduce(rbind, lookup) %>%
+    dplyr::distinct()
+  
+  lookup
+}
+
+make_go_lookup
--- read_data.R
+++ read_data.R
@@ -1,20 +1,28 @@
-library(dplyr)
 
-aliases <- read.table("data/BIOGRID-ORGANISM-Homo_sapiens-3.1.91.tab.txt",
-                 stringsAsFactors = FALSE, skip = 35, header = TRUE, sep = "\t",
-                 comment.char = "")
+get_interactions <- function() {
+  read.table("data/BIOGRID-ORGANISM-Homo_sapiens-3.1.91.tab.txt",
+             stringsAsFactors = FALSE, skip = 35, header = TRUE, sep = "\t",
+             comment.char = "")
+}
 
 
-interactions <- read.table("data/gene_association.goa_human.txt",
+get_ontologies <- function() {
+  ontologies <- read.table("data/gene_association.goa_human.txt",
                            stringsAsFactors = FALSE, skip = 23, header = FALSE, sep = "\t",
                            comment.char = "")
+  
+  # README is here ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/HUMAN/README
+  
+  headers <- readLines("ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/HUMAN/README")
+  headers <-headers[(grep("^GAF2.1", headers) + 3):(grep("^GPAD1.1", headers) - 2)]
+  headers <- sub("\\t\\d{1,2}\\s{1,7}", "", headers)
+  headers <- gsub("\\(|\\)|:", "_", headers)
+  headers <- gsub(" ", "", headers)
+  
+  names(ontologies) <- headers
 
-# README is here ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/HUMAN/README
+  ontologies
+}
 
-headers <- readLines("ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/HUMAN/README")
-headers <-headers[(grep("^GAF2.1", headers) + 3):(grep("^GPAD1.1", headers) - 2)]
-headers <- sub("\\t\\d{1,2}\\s{1,7}", "", headers)
-headers <- gsub("\\(|\\)|:", "_", headers)
-headers <- gsub(" ", "", headers)
 
-names(interactions) <- headers
+
