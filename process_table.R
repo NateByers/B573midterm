3fa532feb363ac4301c424d8eaee9901b20396bc
Nate Byers <natebyers@gmail.com>
2017-10-31 19:05:30 +0000

    use improved get_protein_function

--- process_table.R
+++ process_table.R
@@ -5,54 +5,13 @@ source("make_lookup.R")
 source("make_table.R")
 source("get_protein_function.R")
 
-process_table <- function(table = make_table(), parallel = FALSE, clusters) {
-
-  table <- table %>%
-    dplyr::group_by(interaction)
+process_table <- function(table = make_table()) {
   
-  if(parallel) {
-    if(missing(clusters)) {
-      clusters <- parallel::detectCores() - 1
-    }
-    cl <- parallel::makeCluster(clusters)
-    clusterEvalQ(cl, {
-      library(dplyr)
-      NULL
-    })
-    protein_dfs <- parallel::parLapply(cl, unique(table$official_symbol), function(protein, table) {
-      # protein <- unique(table$official_symbol)[1]
-      table %>%
-        dplyr::filter(protein %in% official_symbol) %>%
-        dplyr::filter(official_symbol != protein) %>%
-        dplyr::group_by(protein_function) %>%
-        dplyr::summarize(n = n()) %>%
-        dplyr::filter(n == max(n)) %>%
-        dplyr::top_n(1, protein_function)
-    }, table = table)
-    stopCluster(cl)
-  } else {
-    then <- Sys.time()
-    protein_dfs <- lapply(unique(table$official_symbol)[1:10], function(protein) {
-      # protein <- unique(table$official_symbol)[1]
-      table %>%
-        dplyr::filter(protein %in% official_symbol) %>%
-        dplyr::filter(official_symbol != protein) %>%
-        dplyr::group_by(protein_function) %>%
-        dplyr::summarize(n = n()) %>%
-        dplyr::filter(n == max(n)) %>%
-        dplyr::top_n(1, protein_function)
-    })
-    Sys.time() - then
-  }
-    
-  Reduce(rbind, protein_dfs)
+  then <- Sys.time()
+  protein_dfs <- lapply(unique(table$official_symbol)[1:10], get_protein_function,
+                        table = table)
+  Sys.time() - then
   
+  protein_df <- Reduce(rbind, protein_dfs)
   
 }
-
-
-# process_table <- function(table = make_table(), lookup = make_lookup()) {
-#   
-#   table <- table %>%
-#     dplyr::group_by(interaction)
-# }
