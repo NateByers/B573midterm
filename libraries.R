30199c01a9b7c8c5e1b6313a79ccad13369f4ce6
Nate Byers <natebyers@gmail.com>
2017-11-01 01:07:28 +0000

    install libraries

---
+++ libraries.R
@@ -0,0 +1,17 @@
+lines <- readLines("helpers.R")
+lines <- grep("::", lines, value = TRUE)
+libraries <- sapply(lines, function(x) {
+  library_ <- strsplit(x, split = "::")[[1]][1]
+  if(grepl("\\s{1,}", library_)) {
+    library_ <- strsplit(library_, "\\s{1,}")[[1]]
+    library_ <- tail(library_, 1)
+  }
+  library_
+  })
+
+for(i in unique(libraries)) {
+  if(require(i, character.only = TRUE)){
+    install.packages(i)
+  }
+  library(i, character.only = TRUE)
+}
